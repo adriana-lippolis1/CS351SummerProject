f67bbfd70251e92189cc8490a0926fab7cbab9fd
adriana-lippolis1 <adriana.lippolis1@gmail.com>
2025-08-14 12:20:23 -0400

    Test-1

---
+++ test-1.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Homepage</title>
+</head>
+<body>
+  <h1>Welcome to My Homepage</h1>
+  <p>This is a very basic homepage.</p>
+  <button onclick="alert('You clicked the button!')">Click Me</button>
+</body>
+</html>
